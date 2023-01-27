389e547073dfe83439ef8d53ece747eaf4213446
Bartosz Piekarczyk <barpie02995@gmail.com>
2023-01-27 13:44:03 +0100

    Initial commit

---
+++ src/main/java/pl/coderslab/Application.java
@@ -0,0 +1,7 @@
+package pl.coderslab;
+
+public class Application {
+    public static void Application(String[] args) {
+        main();
+    }
+}
--- src/main/java/pl/coderslab/Main.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab;
-import static pl.coderslab.ConsoleColors.*;
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
